7816f292e25e4036155c324a710a32529b835cab
foxical <tamlhfoxical@gmail.com>
2017-01-20 17:58:32 +0800

    add libgles

--- android/sdk/lolila_sdk/src/main/cpp/native-lib.cpp
+++ android/sdk/lolila_sdk/src/main/cpp/native-lib.cpp
@@ -1,5 +1,9 @@
 #include <jni.h>
 #include <string>
+#include <GLES3/gl3.h>
+#include <android/asset_manager_jni.h>
+#include <android/log.h>
+
 
 extern "C"
 jstring
@@ -7,5 +11,8 @@ Java_com_foxical_lolila_sdk_IndexApi_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
+
+
+
     return env->NewStringUTF(hello.c_str());
 }
